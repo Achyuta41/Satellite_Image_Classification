287f9d1e77ea52615e374cab921fcc63a7a5878c
Achyuta41 <Achyutak41@gmail.com>
2025-03-30 13:27:02 +0530

    feat:Second changes

--- pred.py
+++
@@ -1,8 +0,0 @@
-import glob
-from ultralytics import YOLO 
-model= YOLO("best.pt")
-image_files = glob.glob("D:\projects_github\Satellite_Image_Classification\data\TEST\*.[j][p][g]")
-#print(image_files)
-for image in image_files:
-   model.predict(source=image,show=True,save=True)
-   print("ok")
